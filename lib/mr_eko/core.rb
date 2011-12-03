c625c3d51210e1aa26d388f1f5e7b6688998c7a5
Ed Hickey <bassnode@gmail.com>
2011-12-02 23:23:09 -0800

    Fix timestamps

--- lib/mr_eko/core.rb
+++ lib/mr_eko/core.rb
@@ -4,6 +4,10 @@ module MrEko
     def self.included(base)
       base.send(:include, GlobalHelpers)
       base.extend GlobalHelpers
+
+      # The Sequel timestamps plugin doesn't seem to work, so...
+      def before_create; self.created_on = Time.now.utc; end
+      def before_update; self.updated_on = Time.now.utc; end
     end
 
     module GlobalHelpers
