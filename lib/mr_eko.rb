092dcdd641db297d89920e17f9ea5fff3ff79bb0
Ed Hickey <bassnode@gmail.com>
2011-12-02 23:56:41 -0800

    minor version bump

--- lib/mr_eko.rb
+++ lib/mr_eko.rb
@@ -19,7 +19,7 @@ EKO_ENV = ENV['EKO_ENV'] || 'development'
 Sequel.default_timezone = :utc
 
 module MrEko
-  VERSION = '0.3.3'
+  VERSION = '0.3.4'
   USER_DIR = File.join(ENV['HOME'], ".mreko")
   FINGERPRINTS_DIR = File.join(USER_DIR, 'fingerprints')
   LOG_DIR = File.join(USER_DIR, 'logs')
